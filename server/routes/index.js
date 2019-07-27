bcfe775a3a535035463a0a699a94c459a08bf5af
Soham Mondal <soham.pf.mondal@gmail.com>
2019-07-27 10:03:51 +0530

    minor adjustments and bug fixes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,29 +1,26 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const Url = require('../models/Url');
+const Url = require("../models/Url");
 
 /*
-* @route GET /:code
-* @desc  Redirect to long/original URL
-*/
+ * @route GET /:code
+ * @desc  Redirect to long/original URL
+ */
 
-router.get('/:code', async (req,res)=>{
-    try{
-        const url = await Url.findOne({ urlCode: req.params.code })
+router.get("/:code", async (req, res) => {
+  try {
+    const url = await Url.findOne({ urlCode: req.params.code });
 
-        if(url){
-
-            return res.redirect(url.longUrl);
-        }else{
-            
-            return res.status(404).json('No url found');
-        }
-
-    }catch(err){
-        console.error(err);
-        res.status(500).json('Server Error');
+    if (url) {
+      return res.redirect(url.longUrl);
+    } else {
+      return res.status(404).json("No url found");
     }
-})
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).json("Server Error");
+  }
+});
 
 module.exports = router;
--- server/routes/url.js
+++ server/routes/url.js
@@ -1,55 +1,51 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const validUrl = require('valid-url');
-const shortid = require('shortid');
-const config = require('config');
+const validUrl = require("valid-url");
+const shortid = require("shortid");
 
-const Url = require('../models/Url');
+const Url = require("../models/Url");
 
 // @route POST /api/url/shorten
 // @desc Create a short URL
 
-router.post('/shorten',async (req,res)=>{
-    const { longUrl } = req.body;
-    const baseUrl = config.get('baseUrl');
+router.post("/shorten", async (req, res) => {
+  const { longUrl } = req.body;
+  const baseUrl = process.env.baseUrl;
 
-    // Check base url
-    if(!validUrl.isUri(baseUrl)){
-        return res.status(401).json('Invalid base url');
+  // Check base url
+  if (!validUrl.isUri(baseUrl)) {
+    return res.status(401).json("Invalid base url");
+  }
+
+  // Create url code
+  const urlCode = shortid.generate();
+
+  // Check long url
+  if (validUrl.isUri(longUrl)) {
+    try {
+      // Check if Url exists in Database
+      let url = await Url.findOne({ longUrl });
+
+      if (url) {
+        res.send(url);
+      } else {
+        const shortUrl = baseUrl + "/" + urlCode;
+        url = new Url({
+          longUrl,
+          shortUrl,
+          urlCode,
+          date: new Date()
+        });
+        await url.save();
+        return res.json(url);
+      }
+    } catch (err) {
+      console.error(err.message);
+      return res.status(500).json("Server Error");
     }
-
-    // Create url code
-    const urlCode = shortid.generate();
-
-    // Check long url
-    if(validUrl.isUri(longUrl)){
-        try {
-            // Check if Url exists in Database
-            let url = await Url.findOne({ longUrl });
-
-            if(url){
-                res.send(url);
-            }else{
-                const shortUrl = baseUrl + '/' + urlCode;
-                url = new Url({
-                    longUrl,
-                    shortUrl,
-                    urlCode,
-                    date: new Date()
-                })
-                await url.save();
-                return res.json(url);
-            }
-
-        }catch(err){
-            console.error(err.message);
-            res.status(500).json('Server Error');
-        }
-        return res.status(401).json('Invalid base url');
-    }else{
-        return res.status(401).json('Invalid url');
-    }
-
-})
+  } else {
+    return res.status(400).json("Invalid url");
+  }
+});
 
 module.exports = router;
