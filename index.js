a12b9d2f1451dd1017cd1ea736cd12a301aa1ce6
Soham Mondal <soham.pf.mondal@gmail.com>
2019-07-27 13:52:20 +0530

    Added Prod code

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
-const express=require('express'),
-      app = express(),
-      cors = require('cors'),
-      PORT = process.env.PORT || 5000,
-      connectDB = require('./config/db');
+const express = require("express"),
+  app = express(),
+  cors = require("cors"),
+  mongoose = require("mongoose"),
+  PORT = process.env.PORT || 5000,
+  mongoURI = process.env.mongoURI;
 
-connectDB();
+// Connect to Mongo
+mongoose
+  .connect(mongoURI, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+  }) // Adding new mongo url parser
+  .then(() => console.log("MONGODB CONNECTED..."))
+  .catch(err => console.log(err));
+
 app.use(cors());
 app.use(express.json());
-app.use('/',require('./routes/index'));
-app.use('/api/url',require('./routes/url'));
-app.listen(PORT,()=>console.log('SERVER LISTENING ON PORT',PORT));
 
+// Use Routes
+app.use("/", require("./routes/index"));
+app.use("/api/url", require("./routes/url"));
 
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+app.listen(PORT, () => console.log("SERVER LISTENING ON PORT", PORT));
