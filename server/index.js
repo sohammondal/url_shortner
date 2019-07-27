5bc605810faefd9d80e35d1b27514301aea9f316
Soham Mondal <soham.pf.mondal@gmail.com>
2019-07-27 10:04:32 +0530

    removed config pkg dependency, added nodemon dev dependency

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 const express=require('express'),
-      app = express();
+      app = express(),
+      cors = require('cors'),
       PORT = process.env.PORT || 5000,
       connectDB = require('./config/db');
 
 connectDB();
-
+app.use(cors());
 app.use(express.json());
 app.use('/',require('./routes/index'));
 app.use('/api/url',require('./routes/url'));
